8949b4a23d90d268cc5f9ac67e7d6fd73aa05a11
piumi96 <piumidp96@gmail.com>
2018-09-18 17:30:56 +0530

    search by district route added.

--- api/routes/search.js
+++ api/routes/search.js
@@ -27,4 +27,31 @@ router.get('/search', (req, res) => {
     });
 });
 
+router.get('/searchbydistrict', (req, res) => {
+    var district = req.body.district;
+    //var district = 'gampaha';
+    var sql = "select * from tutor where Location like '%"+district+"%'";
+
+    con.query(sql, function(err, result){
+        if(err) throw err;
+        else{
+            var user = [];
+            for(var i=0; i<result.length; i++){
+                user[i] = {
+                    fname: result[i].FirstName,
+                    lname: result[i].LastName,
+                    location: result[i].Location,
+                    mobile: result[i].Mobile,
+                    subject: result[i].Subject,
+                    email: result[i].email,
+                    role: result[i].role
+                }
+            }
+            res.send({
+                user: user
+            });
+        }
+    })
+});
+
 module.exports = router;
